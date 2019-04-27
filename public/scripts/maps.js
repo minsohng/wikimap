4680ee176ca87fd4fa3ed7b44a2b495601ee870d
procadiana <diana.claudia.ilinca@gmail.com>
2019-04-27 23:24:56 +0000

    final changes

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -26,6 +26,8 @@ function initialize() {
     var lng = "<b>Longitude: </b>"+ place.geometry.location.lng() + "</br>";
     console.log(place.geometry.location.lat(), place.geometry.location.lng())
     document.getElementById('address_text').innerHTML = location;
+    $('#lat').val(place.geometry.location.lat());
+    $('#lng').val(place.geometry.location.lng());
   })
   initMap()
 }
