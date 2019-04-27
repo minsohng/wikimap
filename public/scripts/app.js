6d21fc3764190399a43f88df869220a352af821b
Thomas Wenner <thmswenner@gmail.com>
2019-04-27 00:53:54 +0000

    fixed ajax error

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,14 +24,3 @@ $(() => {
     })
 
 })
-
-      var input = document.getElementById('pac-input');
-      var searchBox = new google.maps.places.SearchBox(input);
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-      function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13,
-          mapTypeId: 'roadmap'
-        });
