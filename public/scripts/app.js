07af73336c5a3e42c616fb42090773af186bbd4a
Thomas Wenner <thmswenner@gmail.com>
2019-04-27 00:52:09 +0000

    Adjust form layout and add jquery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 $(() => {
 
   // Ajax Request for mapId and run function
-  $('button').on('click', (event) => {
+  $('map-select').on('click', (event) => {
     $.get(`/maps/${event.target.attributes.mapId.value}`, (map_event) => {
       initMap(map_event)
     })
@@ -10,12 +10,28 @@ $(() => {
 
   // Drop down list for selecting a map
 
-  function loadmaps () {
-    console.log($.get('/maps'))
-  }
 
-  $('.drop-down').on('click', event => {
+    $('.drop-down-item').hide()
 
-  })
+    $('.drop-down').on('click', (event) => {
+      $('.drop-down-item').slideToggle(400)
+    })
+
+
+    $('.drop-down-item').on('click', (event) => {
+      $('#choose-map').slideUp(400)
+      $('.event-address').slideDown(400)
+    })
 
 })
+
+      var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      function initAutocomplete() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
+        });
