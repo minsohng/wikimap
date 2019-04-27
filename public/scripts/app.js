75ca5d13ae05b6720a27e42f57c348e3024fd3dd
Thomas Wenner <thmswenner@gmail.com>
2019-04-27 02:05:15 +0000

    Fix jquery event listener

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,4 +23,20 @@ $(() => {
       $('.event-address').slideDown(400)
     })
 
+    function editEvent (data){
+      let $map = $('#map');
+      let $form = $("<div>").addClass("content");
+      let $edit_title = $("<h3>").addClass("edit_title").text("Edit your event data:").appendTo($form);
+      let $form_latitude = $("<input>").addClass("form_latitude").appendTo($form);
+      let $form_longitude = $("<input type: 'value', id: 'flong', placeholder: 'Longitude'/>").appendTo($form);
+      let $form_name = $("<input type: 'text', id: 'fname', Event name: 'name', placeholder: 'Event Name'/>").appendTo($form);
+      let $form_start = $("<input>").addClass("form_start").appendTo($form);
+      let $form_end = $("<input>").addClass("form_end").appendTo($form);
+      let $form_url = $("<input>").addClass("form_url").appendTo($form);
+      let $form_picture = $("<input>").addClass("form_picture").appendTo($form);
+      let $f_description = $("<textarea rows: 'px', cols: '27px',type: 'text',id: 'description', name: 'desc', placeholder: 'Event description' />").appendTo($form);
+      let $submit_bttn = $("<input type: 'button', id: 'submit', placeholder: 'Submit' />").appendTo($form);
+      $form.appendTo($map);
+      return $form.prop('outerHTML');
+    }
 })
